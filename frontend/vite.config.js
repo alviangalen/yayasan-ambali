b408b5432cee12716bb55ace051d42e7eef85305
Galen Alvian <galen@LAPTOP-GALEN.localdomain>
2026-04-16 10:22:39 +0700

    fix api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   envDir: '../',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
   server: {
     proxy: {
       '/api': {
