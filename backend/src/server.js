3e9edc6b229945bf71356840eb968fdec871db03
Faris <alfarisiazmir14@gmail.com>
2026-04-24 21:40:36 +0700

    akal2an barat

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
